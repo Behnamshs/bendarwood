c76738f759e57618a0aabe008d5459658cd2770f
Behnamshs <165054650+Behnamshs@users.noreply.github.com>
2025-10-16 19:29:50 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+  window.addEventListener("load", () => {
+    const slider = document.getElementById("slider");
+
+    // اول اسکرول رو از ابتدا صفر کن
+    slider.scrollLeft = 0;
+
+    // بعد از یه لحظه (برای اینکه المنت لود شه) شروع به حرکت کن
+    setTimeout(() => {
+      slider.scrollTo({
+        left: slider.scrollWidth,
+        behavior: "smooth"
+      });
+    }, 300); // ۳۰۰ میلی‌ثانیه تأخیر برای بارگذاری کامل
+  });
